b7537a87423bee4efa8da17dd5484b04443a4c17
onedanfc <onedanfc@gmail.com>
2023-01-08 20:19:19 +0700

    Pesan commit

---
+++ flexbox.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flexbox</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <h1><a href="index.php" style="text-decoration: none; color: #ffffff">HOME</a></h1>
+        <ul>
+            <li><a href="grid.php" class="button">Grid</a></li>
+            <li><a href="flexbox.php" class="button">Flexbox</a></li>
+            <li style="margin-left: 2em" class="login">Login</li>
+        </ul>
+    </nav>
+    <h1>Flexbox</h1>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Responsive Design</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <nav>
+            <h1><a href="index.php" style="text-decoration: none; color: #ffffff">HOME</a></h1>
+            <ul>
+                <li><a href="grid.php" class="button">Grid</a></li>
+                <li><a href="flexbox.php" class="button">Flexbox</a></li>
+                <li style="margin-left: 2em" class="login">Login</li>
+            </ul>
+        </nav>
+
+        <div>
+            ini adalah sebuah kotak yang digunakan sebagai contoh membuat flexbox
+        </div>
+        <script>
+        </script>
+    </body>
+</html>
